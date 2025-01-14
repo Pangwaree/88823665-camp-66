0d7848d468a44be397e347a48f0c74a96b4c62fe
Pangwaree <66160362@go.buu.ac.th>
2025-01-14 19:39:52 +0700

    template

---
+++ mylaravel/resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>camp 66</title>
+    </head>
+    <body>
+        <h1>Template CAMP 66</h1>
+        <a href="{{url(''}}"></a>
+    </body>
+</html>
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MyController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.default');
 });
 
 Route::get('/hello', function () {
