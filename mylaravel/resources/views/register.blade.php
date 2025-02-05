9fd2bda0ccf68917779e104121175e07d4efaade
Pangwaree <66160362@go.buu.ac.th>
2025-02-05 22:45:03 +0700

    JavaScript

--- mylaravel/resources/views/register.blade.php
+++ mylaravel/resources/views/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="register-page" style="background-color:rgb(206, 216, 255)">
+<div class="register-page" >
     <div class="register-box">
         <div class="register-logo">
           <a href="../index2.html"><b>Admin</b>LTE</a>
@@ -10,7 +10,7 @@
         <div class="card">
           <div class="card-body register-card-body">
             <p class="register-box-msg">Register a new membership</p>
-            <form action="{{ url('/register') }}" onsubmit="return clickme()" method="post">
+            <form action="{{ url('/register') }}" onsubmit="return myfunction();" method="post">
                 @csrf
               <div class="input-group mb-3">
                 <input type="text" name="name" id ="name" class="form-control" placeholder="Full Name" />
@@ -18,10 +18,11 @@
                 <div class ="valid-feedback">
                   ok
                 </div>
-                <div class ="invalid-feedback">
-                  กรุณาระบุข้อมูล name
+                <div class ="invalid-feedback" id="invalid-name">
+                  กรุณาระบุข้อมูล Name
                 </div>
               </div>
+
               <div class="input-group mb-3">
                 <input type="email" name="email" id ="email" class="form-control" placeholder="Email" />
                 <div class="input-group-text"><span class="bi bi-envelope"></span></div>
@@ -34,7 +35,7 @@
               <div class="row">
                 <div class="col-8">
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <input class="form-check-input" id = "mycheckbox" type="checkbox" value="" id="flexCheckDefault" />
                     <label class="form-check-label" for="flexCheckDefault">
                       I agree to the <a href="#">terms</a>
                     </label>
@@ -50,7 +51,7 @@
               </div>
               <!--end::Row-->
             </form>
-           
+           <button class="btn" onclick="myfunction()"> Click me </button>
             <!-- /.social-auth-links -->
             <p class="mb-0">
               <a href="login.html" class="text-center"> I already have a membership </a>
@@ -70,8 +71,8 @@
 
 <script>
   //alert("Hello world!")
-  let myvar
-  var myvar2
+  let myvar //ส่วนใหญ่ใข้
+  var myvar2 //แบบเก่า
   const PI=3.14
   pi = 2
   console.log(PI,pi)
@@ -90,19 +91,40 @@ console.log(myarry)
 for(a=1;a<10;a++){
   console.log(a)
 }
-function clickme(){
-  document.getElementById('name');
-    name.value = "new test"
-    name = $('#name').val("new with jquery")
-    $('name').addClass('is-invalid') //error
+
+//function clickme(){
+  //document.getElementById('name');
+    //name.value = "new test"
+    //name = $('#name').val("new with jquery") //เรียกidใช้#
+    //$('name').addClass('is-invalid') //error
     //$('name').addClass('is-valid') //ok
     //email format @[a-z].[a-z]
     //password format [0-9][a-z][A-Z]
-  return false;
+  //return false;
+//}
+
+function myfunction(){
+  let name = document.getElementById('name')
+  name = $('#name')
+  let email = document.getElementById('email')
+  let password = document.getElementById('password')
+  let mycheckbox = document.getElementById('mycheckbok')
+  //name.value = "My Name Value"
+  //name.val("My Name Value")
+  console.log(name.val(), email.value, password.value, mycheckbox.checked)
+  if(name.val() == " "){
+    name.addClass('is-invalid');
+    $('#invalid-name').html("<b> กรุณากรอกชื่อ </b>") //เป็นการเปลี่ยนข้อความ
+    return false; //ถ้าค่าผิดจะไม่ส่งค่า
+  }else{
+    name.removeClass('is-invalid');
+  }
+ return true;
 }
-$(document).ready(function(){
+
+//$(document).ready(function(){
   //alert("Hello world!")
-})
+//})
 </script>
 
 
--- mylaravel/resources/views/user/index.blade.php
+++ mylaravel/resources/views/user/index.blade.php
@@ -2,82 +2,99 @@
 
 @section('content')
 <div class="row">
-              <div class="col-md-12">
-                <div class="card mb-12">
-                  <div class="card-header"><h3 class="card-title">User Tables</h3></div>
-                  <!-- /.card-header -->
-                  <div class="card-body">
-                    <table class="table table-bordered">
-                    <thead class="table-primary">
-                        <tr>
-                          <th style="width: 10px">#</th>
-                          <th>Name</th>
-                          <th>Email</th>
-                          <th style="width: 240px">Option</th>
-                        </tr>
-                      
-                      <tbody>
-                        <?php foreach($users as $index => $user) {?>
-                        <tr class="align-middle">
-                          <td>{{ $index+1 }}.</td>
-                          <td>{{ $user->name }}</td>
-                          <td>{{$user->email}}</td>
-                          <td>
-                            <a href="{{url('/user/'.$user->id)}}">
-                            <button class="btn btn-warning">Edit</button>
-                            </a>
-                            <form action="{{url('/user')}}" onsubmit = "return clickme()" method="post" style="display:inline;">
-                              @csrf
-                              @method('delete')
-                            <input type="hidden" name="id" value="{{ $user->id}}">
-                            <button type ="submit"class="btn btn-danger">Delete</button>
-                          </form>
-                          </td>
-                        </tr>
-                          <?php } ?>
-                      </tbody>
-                    </thead>
-                    </table>
-                  </div>
-                  <!-- /.card-body -->
-                  <div class="card-footer clearfix">
-                    <ul class="pagination pagination-sm m-0 float-end">
-                      <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <!-- /.card -->
-      
-              </div>
+  <div class="col-md-12">
+    <div class="card mb-12">
+      <div class="card-header">
+        <h3 class="card-title">User Tables</h3>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body">
+        <table class="table table-bordered">
+          <thead class="table-primary">
+            <tr>
+              <th style="width: 10px">#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th style="width: 240px">Option</th>
+            </tr>
+
+          <tbody>
+            <?php foreach ($users as $index => $user) { ?>
+              <tr class="align-middle">
+                <td>{{ $index+1 }}.</td>
+                <td>{{ $user->name }}</td>
+                <td>{{$user->email}}</td>
+                <td>
+                  <a href="{{url('/user/'.$user->id)}}">
+                    <button class="btn btn-warning">Edit</button>
+                  </a>
+                  <form action="{{url('/user')}}" onsubmit="return confirm_delete()" method="post" style="display:inline;">
+                    @csrf
+                    @method('delete')
+                    <input type="hidden" name="id" value="{{ $user->id}}">
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                  </form>
+                </td>
+              </tr>
+            <?php } ?>
+          </tbody>
+          </thead>
+        </table>
+      </div>
+      <!-- /.card-body -->
+      <div class="card-footer clearfix">
+        <ul class="pagination pagination-sm m-0 float-end">
+          <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- /.card -->
+
+  </div>
 </div>
 @endsection
 
 @section('scripts')
 <script>
-  function clickme(){
-    Swal.fire({
-  title: "Are you sure?",
-  text: "Delete it or not",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#3085d6",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Yes, delete it!"
-}).then((result) => {
-  if (result.isConfirmed) {
-   return true;
+  function confirm_delete(){  //อีกแบบ confirm_delete = function(){ }
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => { //result คือ function
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire({
+          title: "Deleted!",
+          text: "Your User has been deleted.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your User ist'n delete :)",
+          icon: "error"
+        });
+      }
+    });
     }
-    return false;
-});
-return false;
-  }
-$(document).ready(function(){
-
-})  
 </script>
 
 
